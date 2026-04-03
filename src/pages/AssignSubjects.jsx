e3ac49bc97d19de6650c7baeeeb6089f6c5d75bb
23f3001176 <23f3001176@ds.study.iitm.ac.in>
2026-04-03 22:39:05 +0530

    Fixed mixed content error by adding https to backend URL

--- src/pages/AssignSubjects.jsx
+++ src/pages/AssignSubjects.jsx
@@ -30,7 +30,7 @@ export default function AssignSubjects() {
 
   const fetchData = async () => {
     const token = localStorage.getItem("token");
-    const baseUrl = "http://sams-zsar.onrender.com:8000";
+    const baseUrl = "https://sams-zsar.onrender.com:8000";
 
     try {
       setLoading(true);
